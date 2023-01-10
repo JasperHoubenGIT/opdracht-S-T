a4fac98bfa7af18883840918901d1e30b2b34345
JasperHoubenGIT <jasperhouben@gmail.com>
2023-01-10 10:57:54 +0100

    opdracht 3 voltooid

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 /** Voeg hier je code toe */
+
+
+function addBox() {
+    const container = document.getElementById('assignment-03');
+    const box = document.createElement('div');
+    var x = document.getElementsByTagName('div').length - 9;
+
+    box.setAttribute('class', 'block');
+    if (x <= 10){box.textContent = x}else (alert("you can only add 10 blocks nol ;) "));
+
+    container.appendChild(box);
+
+
+    console.log(x)
+}
+
+
+
+
+
+
+
